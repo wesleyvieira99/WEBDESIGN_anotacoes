04a3086df8b2a77b82e62d8c67a7aca6374f37a4
Wesley Vieira <75049150+wesleyvieira99@users.noreply.github.com>
2021-08-12 17:03:37 -0300

    Add files via upload

---
+++ jquery/PROJETOS/js/temporizador.js
@@ -0,0 +1,55 @@
+$.fn.temporizador = function(opcoes) {
+    //valor padrão, caso ninguem passe nada nos parametros
+    const opcoesFinais = $.extend({
+        mensagem: 'Em breve',
+        horario: '23:59:59'
+    }, opcoes)
+
+    const horaDezena = $('<span class="digito">').html('0')
+    const horaUnidade = $('<span class="digito">').html('0')
+    const minutoDezena = $('<span class="digito">').html('0')
+    const minutoUnidade = $('<span class="digito">').html('0')
+    const segundoDezena = $('<span class="digito">').html('0')
+    const segundoUnidade = $('<span class="digito">').html('0')
+
+    const separadorHora = $('<span class="separador">').html(':')
+    const separadorMinuto = $('<span class="separador">').html(':')
+    const mensagem = $('<div class="mensagem">').html(opcoesFinais.mensagem)
+
+    $(this).addClass('temporizador')
+    $(this).append(horaDezena, horaUnidade, separadorHora, minutoDezena, minutoUnidade, separadorMinuto, segundoDezena, segundoUnidade, mensagem)
+
+    //padrão regex para captar horas
+    const regex = new RegExp(/(\d\d):(\d\d):(\d\d)/)
+
+    //horario alvo - aqui eu consigo pegar separadamente a hora, o minuto e o segundo
+    const horarioAlvo = regex.exec(opcoesFinais.horario) 
+
+    let temporizador = setInterval(() => {
+        const agora = new Date()
+        const alvo = new Date()
+        alvo.setHours(horarioAlvo[1])
+        alvo.setMinutes(horarioAlvo[2])
+        alvo.setSeconds(horarioAlvo[3])
+
+        const diferencaEmMili = alvo.getTime() - agora.getTime()
+
+        if (diferencaEmMili >= 0) {
+            //regex para extrair a diferença das datas em horas, minutos e segundos
+            const diferenca = regex.exec(new Date(diferencaEmMili).toISOString())
+
+            horaDezena.html(diferenca[1][0])
+            horaUnidade.html(diferenca[1][1])
+
+            minutoDezena.html(diferenca[2][0])
+            minutoUnidade.html(diferenca[2][1])
+
+            segundoDezena.html(diferenca[3][0])
+            segundoUnidade.html(diferenca[3][1])
+        } else {
+            clearInterval(temporizador)
+        }
+    }, 1000)
+
+    return this
+}

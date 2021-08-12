0e2b692b51a5230bdf5dd47ad32a7641dd2ebf68
Wesley Vieira <75049150+wesleyvieira99@users.noreply.github.com>
2021-08-12 16:53:59 -0300

    Add files via upload

---
+++ ANGULAR/frontend/src/app/components/product/product.service.ts
@@ -0,0 +1,48 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Observable } from 'rxjs';
+import { Product } from './product.model';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  constructor(private snackBar: MatSnackBar, private http:HttpClient) { }
+
+  baseUrl = "http://localhost:3001/products"
+
+  showMessage(msg: string): void {
+    this.snackBar.open(msg, 'X', {
+      duration: 3000,
+      horizontalPosition: "right",
+      verticalPosition: "top"
+    })
+  }
+
+  create(product: Product): Observable<Product> {
+    return this.http.post<Product>(this.baseUrl, product)
+  }
+
+  read(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.baseUrl)
+  }
+
+  readById(id: string): Observable<Product> {
+    const url = `${this.baseUrl}/${id}`
+    return this.http.get<Product>(url)
+  }
+
+  update(product: Product): Observable<Product> {
+    const url = `${this.baseUrl}/${product.id}`
+    return this.http.put<Product>(url, product)
+  }
+
+  delete(id: number): Observable<Product> {
+    const url = `${this.baseUrl}/${id}`
+    return this.http.delete<Product>(url);
+  }
+
+}
